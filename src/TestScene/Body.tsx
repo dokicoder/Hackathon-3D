5c68bd86e488476ccb52615183689c84d6e036e6
Kay Illner <kay.illner@ti8m.ch>
2023-08-22 15:26:20 +0200

    highlight body region on hover

--- src/TestScene/Body.tsx
+++ src/TestScene/Body.tsx
@@ -1,6 +1,6 @@
 import { useGLTF } from '@react-three/drei';
-import { createRef, useState } from 'react';
-import { Group, MeshBasicMaterial, Vector3 } from 'three';
+import { createRef, useRef, useState } from 'react';
+import { Group, Material, Mesh, MeshStandardMaterial, Vector3 } from 'three';
 import { MeshProps } from '@react-three/fiber';
 import bodyUrl from '../assets/male_body_separated.glb?url';
 
@@ -29,24 +29,24 @@ export const Body = () => {
   const el = useGLTF(bodyUrl);
   const ref = createRef<Group>();
 
-  console.log('Body', el);
-
+  /*
   const selectionMaterial: MeshBasicMaterial = (
     el as any
   ).nodes.foot_left.material.clone();
-
   selectionMaterial.setValues({ color: '#ff6644' });
-
   (el as any).nodes.foot_left.material = selectionMaterial;
   (el as any).nodes.foot_right.material = selectionMaterial;
   (el as any).nodes.hand_left.material = selectionMaterial;
   (el as any).nodes.hand_right.material = selectionMaterial;
+  */
 
   const [previewPosition, setPreviewPosition] = useState<Vector3 | undefined>(
     new Vector3(0, 0, 0)
   );
   const [showPreview, setShowPreview] = useState(true);
   const [woundStates, setWoundStates] = useState<IWoundState[]>([]);
+
+  const hoverMaterialRef = useRef<MeshStandardMaterial>();
 
   return (
     <group ref={ref}>
@@ -70,6 +70,34 @@ export const Body = () => {
               ...woundStates,
               { position: previewPosition, toggled: true },
             ]);
+          }
+        }}
+        onPointerEnter={(e) => {
+          e.stopPropagation();
+          console.log(e);
+
+          if (hoverMaterialRef.current) {
+            hoverMaterialRef.current.dispose();
+          }
+          hoverMaterialRef.current = (e.object as Mesh)
+            .material as MeshStandardMaterial;
+
+          const coloredMaterial = (
+            (e.object as Mesh).material as MeshStandardMaterial
+          ).clone();
+
+          coloredMaterial.setValues({ color: '#aaffaa' });
+
+          (e.object as Mesh).material = coloredMaterial;
+        }}
+        onPointerLeave={(e) => {
+          e.stopPropagation();
+
+          if (hoverMaterialRef.current) {
+            // TODO: this could go bad
+            ((e.object as Mesh).material as Material).dispose();
+
+            (e.object as Mesh).material = hoverMaterialRef.current;
           }
         }}
       >
