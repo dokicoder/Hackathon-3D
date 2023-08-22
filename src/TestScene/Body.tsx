3fa1cefd561b4d2068db86bb99866a9d579ae55a
Martin Schlunegger <m.schlunegger@gmail.com>
2023-08-22 14:52:16 +0200

    feat: gltf to jsx

--- src/TestScene/Body.tsx
+++ src/TestScene/Body.tsx
@@ -1,8 +1,8 @@
-import { useGLTF } from '@react-three/drei';
-import { createRef, useState } from 'react';
-import { Group, MeshBasicMaterial, Vector3 } from 'three';
-import { MeshProps } from '@react-three/fiber';
-import bodyUrl from '../assets/male_body_separated.glb?url';
+import { useGLTF } from "@react-three/drei";
+import { createRef, useState } from "react";
+import { Group, MeshBasicMaterial, Vector3 } from "three";
+import { MeshProps } from "@react-three/fiber";
+import bodyUrl from "../assets/male_body_separated.glb?url";
 
 interface ISphereProps extends MeshProps {
   opacity: number;
@@ -27,15 +27,17 @@ useGLTF.preload(bodyUrl);
 
 export const Body = () => {
   const el = useGLTF(bodyUrl);
+
+  const { nodes, materials } = useGLTF(bodyUrl) as any;
   const ref = createRef<Group>();
 
-  console.log('Body', el);
+  console.log("Body", el);
 
   const selectionMaterial: MeshBasicMaterial = (
     el as any
   ).nodes.foot_left.material.clone();
 
-  selectionMaterial.setValues({ color: '#ff6644' });
+  selectionMaterial.setValues({ color: "#ff6644" });
 
   (el as any).nodes.foot_left.material = selectionMaterial;
   (el as any).nodes.foot_right.material = selectionMaterial;
@@ -73,15 +75,58 @@ export const Body = () => {
           }
         }}
       >
-        <primitive object={el.scene} />
+        <group dispose={null}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Body_low_Body_low_0.geometry}
+            material={materials.Body_low}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.foot_left.geometry}
+            material={materials.Body_low}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.foot_right.geometry}
+            material={materials.Body_low}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.hand_left.geometry}
+            material={materials.Body_low}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.hand_right.geometry}
+            material={materials.Body_low}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.EyeGlass_low_EyeGlass_low_0.geometry}
+            material={materials.EyeGlass_low}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Male_Basic_Male_Basic_0.geometry}
+            material={materials.Male_Basic}
+          />
+        </group>
       </mesh>
       {previewPosition && showPreview && (
-        <Sphere position={previewPosition} color={'#ffaaaa'} opacity={0.5} />
+        <Sphere position={previewPosition} color={"#ffaaaa"} opacity={0.5} />
       )}
       {woundStates.map(({ position, toggled }, idx) => (
         <Sphere
           position={position}
-          color={toggled ? '#1166ff' : '#ff6644'}
+          color={toggled ? "#1166ff" : "#ff6644"}
           opacity={0.8}
           key={idx}
           onPointerMove={(e) => {
