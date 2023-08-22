2df63753349dc8a1106832986279751063ecda24
Martin Schlunegger <m.schlunegger@gmail.com>
2023-08-22 11:39:44 +0200

    feat: card added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,14 @@
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
 import FilterableTreeView from "./FilterableTreeView";
 
 export const SideBar = (): JSX.Element => {
   return (
-    <div>
-      <h1>Wunde hinzufügen</h1>
-      <FilterableTreeView />
-    </div>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <h1>Wunde hinzufügen</h1>
+        <FilterableTreeView />
+      </CardContent>
+    </Card>
   );
 };
