2223b92775ec7247fb28f8d379b95b578d63a42f
Martin Schlunegger <m.schlunegger@gmail.com>
2023-08-23 18:01:28 +0200

    feat: use view transition

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { flushSync } from 'react-dom';
 
 const loadFileAsData = (file: File, afterLoading: (e: Event) => void) => {
   if (!file) return;
@@ -42,9 +43,25 @@ const FileUploadMultiple = ({
     setIsDragOver(false);
   };
 
+  const handleThumbnailClick = (wound: string | undefined) => {
+    // @ts-ignore -
+    document.startViewTransition(() => {
+      flushSync(() => {
+        setSelectedImage(wound);
+      });
+    });
+  };
+
   return (
     <>
-      <div></div>
+      {selectedImage && (
+        <div
+          onClick={() => handleThumbnailClick(undefined)}
+          className="image-overlay"
+        >
+          <img src={selectedImage} className="wound-image" />
+        </div>
+      )}
       <div
         onDragEnter={handleDragEnter}
         onDragOver={(e) => e.preventDefault()}
@@ -55,10 +72,17 @@ const FileUploadMultiple = ({
         {woundPictures.map((wound, idx) => (
           <div
             className="wound-picture"
-            onClick={() => setSelectedImage(wound)}
+            onClick={() => handleThumbnailClick(wound)}
             key={idx}
           >
-            <img src={wound} key={'w' + idx} width={'100px'} />
+            {!selectedImage && (
+              <img
+                src={wound}
+                key={'w' + idx}
+                width={'100px'}
+                className="wound-image"
+              />
+            )}
           </div>
         ))}
       </div>
