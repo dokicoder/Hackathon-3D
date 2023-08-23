61e2bfb1c7d415a5aac7317b42ecc4a7aa57a3b0
Martin Schlunegger <m.schlunegger@gmail.com>
2023-08-23 17:33:36 +0200

    feat: implement droppable image upload

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { useState } from 'react';
 
 const loadFileAsData = (file: File, afterLoading: (e: Event) => void) => {
   if (!file) return;
@@ -18,23 +18,51 @@ const FileUploadMultiple = ({
   woundPictures: string[];
   setWoundPictures: (woundPictures: string[]) => void;
 }) => {
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      loadFileAsData(e.target.files[0], (eve) => {
+  const handleFileChange = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    if (e.dataTransfer?.files) {
+      loadFileAsData(e.dataTransfer?.files[0], (eve) => {
         setWoundPictures([...woundPictures, (eve.target as any).result]);
       });
     }
   };
 
+  const [isDragOver, setIsDragOver] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const handleDragEnter = (e: any) => {
+    e.preventDefault();
+    setIsDragOver(true);
+  };
+
+  const handleDragLeave = (e: any) => {
+    e.preventDefault();
+    setIsDragOver(false);
+  };
+
   return (
-    <div className="file-upload">
-      <input type="file" onChange={handleFileChange} multiple />
-      {woundPictures.map((wound, idx) => (
-        <div className="wound-picture" key={idx}>
-          <img src={wound} key={'w' + idx} width={'100px'} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div></div>
+      <div
+        onDragEnter={handleDragEnter}
+        onDragOver={(e) => e.preventDefault()}
+        onDragLeave={handleDragLeave}
+        onDrop={(e) => handleFileChange(e)}
+        className="file-upload"
+      >
+        {woundPictures.map((wound, idx) => (
+          <div
+            className="wound-picture"
+            onClick={() => setSelectedImage(wound)}
+            key={idx}
+          >
+            <img src={wound} key={'w' + idx} width={'100px'} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
