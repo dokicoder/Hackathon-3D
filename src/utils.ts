546c9657bfc21e32b6fd8f679c44fe481ad38f94
Kay Illner <kay.illner@ti8m.ch>
2023-08-23 13:59:50 +0200

    fix merge

--- src/utils.ts
+++ src/utils.ts
@@ -5,5 +5,9 @@ export const getLocationFromBodyPart = (bodyPart?: string, fallBack?: string) =>
     foot_left: 'Linker Fuss',
     foot_right: 'Rechter Fuss',
     face_front: 'Gesicht',
+    arm_left_lower_back: 'Linker Unterarm, Rückseite',
+    arm_right_lower_back: 'Rechter Unterarm, Rückseite',
+    arm_left_lower_front: 'Linker Unterarm, Vorderseite',
+    arm_right_lower_front: 'Rechter Unterarm, Vorderseite',
     body: 'Sonstige',
   }[bodyPart as string] || fallBack);
