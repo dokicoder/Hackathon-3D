ccdb3b3eaf2c74dbd8ae83fa16383d721c49c477
Kay Illner <kay.illner@gmail.com>
2023-10-30 12:16:18 +0100

    deploy: vite config base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import eslintPlugin from "vite-plugin-eslint";
-import react from "@vitejs/plugin-react";
-import plainText from "vite-plugin-plain-text";
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+import react from '@vitejs/plugin-react';
+import plainText from 'vite-plugin-plain-text';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Hackathon-3D/',
   plugins: [react(), eslintPlugin(), plainText(/.*.yarn$/)],
   css: {
     postcss: {
       map: false,
       plugins: [
-        require("stylelint")({}),
-        require("postcss-nested"),
-        require("autoprefixer"),
+        require('stylelint')({}),
+        require('postcss-nested'),
+        require('autoprefixer'),
       ],
     },
   },
